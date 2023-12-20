d5190570f86eec011f802bb9c8b68f01ddd93912
Prashant Kumar <prashantkumar0507200@gmail.com>
2023-12-20 14:21:30 +0530

    sendOTP

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -11,11 +11,11 @@ dotenv.config();
 router.post("/sendOTP" , async (req: express.Request, res: express.Response) => {
     let OTP = randomNumber();
     logger(false, `Sending OTP ${OTP} to mobile : ${req.body.mobile}`);
-    const callapi = await sendOTP(parseInt(OTP), req.body.mobile); 
     try {
+        const callapi = await sendOTP(parseInt(OTP), req.body.mobile); 
         res.json({ success: true, message: `OTP sent successfully to Mobile ${req.body.mobile}` });
     } catch(err) {
-        res.json({ success: false, message: `OTP sent Failed to Mobile ${req.body.mobile}..` });
+        res.json({ success: false, message: `OTP sent Failed to Mobile ${req.body.mobile} , Try again later... ${err}` });
     }
 });
 
